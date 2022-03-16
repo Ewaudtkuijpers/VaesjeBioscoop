a4ec52c71b3dfab9f60f062a8d13e528bafe2057
Ewoud Kuijpers <Ewoud110@hotmail.com>
2022-03-16 10:00:23 +0100

    Contact formilier meer itmes toegeoegd

--- VaesjeBioscoop/Models/Person.cs
+++ VaesjeBioscoop/Models/Person.cs
@@ -4,13 +4,19 @@ namespace VaesjeBioscoop.Models
 {
     public class Person
     {
+        [Required]
         public string Firstname { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
         [Required]
         public string Email { get; set; }
 
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
         public string Message { get; set; }
 
     }
